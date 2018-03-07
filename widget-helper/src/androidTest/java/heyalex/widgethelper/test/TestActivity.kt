3feec0c770113daec97565528541809069175b90
a.fialko <a.fialko@evotor.ru>
2018-03-07 15:32:30 +0300

    add tests on extensions

---
+++ widget-helper/src/androidTest/java/heyalex/widgethelper/test/TestActivity.kt
@@ -0,0 +1,11 @@
+package heyalex.widgethelper.test
+
+import android.app.Activity
+import android.os.Bundle
+
+class TestActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.test_activity)
+    }
+}
