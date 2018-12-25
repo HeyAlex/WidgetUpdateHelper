c3c58ec62b9646c76e05af9a7c72e40db4541d7c
a.fialko <a.fialko@corp.mail.ru>
2018-12-25 09:56:17 +0300

    list update widget still in work

--- example/src/main/java/helper/update/widget/com/example/list/ListAppWidget.java
+++ example/src/main/java/helper/update/widget/com/example/list/ListAppWidget.java
@@ -1,10 +1,18 @@
 package helper.update.widget.com.example.list;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.os.Bundle;
 
 import heyalex.widgethelper.RemoteViewsUpdater;
+import heyalex.widgethelper.WidgetUpdateService;
 
-@RemoteViewsUpdater(ListRemoteViewBuilder.class)
+@RemoteViewsUpdater(ListRemoteViewUpdater.class)
 public class ListAppWidget extends AppWidgetProvider {
-
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        WidgetUpdateService.updateWidgets(context,
+                this, new Bundle(), appWidgetIds);
+    }
 }
---
+++ example/src/main/java/helper/update/widget/com/example/list/ListRemoteViewUpdater.java
@@ -0,0 +1,32 @@
+package helper.update.widget.com.example.list;
+
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+
+import helper.update.widget.com.example.R;
+import heyalex.widgethelper.WidgetUpdater;
+
+public class ListRemoteViewUpdater implements WidgetUpdater {
+    @Override
+    public void update(Context context, Bundle dataBundle, int... ids) {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        ids = appWidgetManager.getAppWidgetIds(new ComponentName(context,
+                ListAppWidget.class));
+
+        //update all widgets
+        for (int id : ids) {
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.example_list_app_widget);
+            Intent adapter = new Intent(context, SampleRemoteService.class);
+            Uri data = Uri.parse(adapter.toUri(Intent.URI_INTENT_SCHEME));
+            adapter.setData(data);
+            remoteViews.setRemoteAdapter(R.id.list, adapter);
+            appWidgetManager.updateAppWidget(id, remoteViews);
+            appWidgetManager.notifyAppWidgetViewDataChanged(id, R.id.list);
+        }
+    }
+}
